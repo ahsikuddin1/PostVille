dacad5cf6251fa69c3bf2df16d0fb5cbc2b97401
ahsikuddin1 <67656602+ahsikuddin1@users.noreply.github.com>
2020-10-19 18:14:38 -0400

    Updated migrations, fixed cors/psql errors

--- postville/app/models/post.rb
+++ postville/app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
 
-  validates :username, presence: true
+  validates :user, presence: true
   validates :content, presence: true
 end
--- postville/db/schema.rb
+++ postville/db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_19_170250) do
+ActiveRecord::Schema.define(version: 2020_10_19_214011) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.string "post"
@@ -30,7 +33,7 @@ ActiveRecord::Schema.define(version: 2020_10_19_170250) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
